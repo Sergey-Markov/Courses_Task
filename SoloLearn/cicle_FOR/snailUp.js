c91177e70f3fd4424ac0c7a64042ff1381c05f74
Sergey-Markov <1nativefl@gmail.com>
2020-12-02 10:42:22 +0200

    Задача по циклам про Улитку и колодец

---
+++ SoloLearn/cicle_FOR/snailUp.js
@@ -0,0 +1,12 @@
+var depth = 42;
+var x = 0;
+for (i = 0; i <= depth; i) {
+    i = i +7;
+    x = x+1;
+    if (i >= depth){
+    break;
+    } else {i = i - 2;};
+    
+console.log(i);
+}  
+console.log(x);
