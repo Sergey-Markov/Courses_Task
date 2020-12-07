fdca63167c079123e4d2a1b0a2bb5e2893ac5189
Sergey-Markov <1nativefl@gmail.com>
2020-12-07 09:24:37 +0200

    Задача для определения скидки в магазине через метод объекта

---
+++ SoloLearn/object/saleInMagazine/saleInMagazine.js
@@ -0,0 +1,22 @@
+var prodID = "Лафет1493";
+var price = 1700;
+var discount = 15;
+
+var prod1 = new Product(prodID, price);
+
+document.write('Вывод данных через JS: <br />' + prod1.prodID + " price: " + prod1.price);
+
+//сформируйте скидку
+var discount = 1700 - (1700* (15/100))
+prod1.changePrice(discount);
+
+document.write('<br />' + prod1.prodID + " new price: " + prod1.price);
+
+function Product(prodID, price) {
+    this.prodID = prodID;
+    this.price = price;
+    this.changePrice = function (price) {
+        this.price = price;
+        
+    }
+}
