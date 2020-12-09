78b8c9cc2da31385e9114c2674a7f0a9c0f83bac
Sergey-Markov <1nativefl@gmail.com>
2020-12-09 11:13:22 +0200

    Задачи на изменение масивов 2 варианта решения одной задачи

---
+++ SoloLearn/massive/masive.js
@@ -0,0 +1,25 @@
+//var breackfasts = ['Cinnamon Doughnuts', 'Waffles', 'Granola', 'Chorizo Burrito', 'French Toast'];
+var index = 3;
+if (index == 0) {
+        var breackfasts = ['Fluffy Pancackes', 'Waffles', 'Granola', 'Chorizo Burrito', 'French Toast'];
+        console.log(breackfasts);
+    }
+    if (index == 1) {
+        var breackfasts = ['Cinnamon Doughnuts', 'Fluffy Pancackes', 'Granola', 'Chorizo Burrito', 'French Toast'];
+        console.log(breackfasts);
+    }
+    if (index == 2) {
+        var breackfasts = ['Cinnamon Doughnuts', 'Waffles', 'Fluffy Pancackes', 'Chorizo Burrito', 'French Toast'];
+        console.log(breackfasts);
+    }
+    if (index == 3) {
+        var breackfasts = ['Fluffy Pancackes', 'Waffles', 'Granola', 'Fluffy Pancackes', 'French Toast'];
+        document.write(breackfasts);
+    }if (index == 4) {
+        var breackfasts = ['Cinnamon Doughnuts', 'Fluffy Pancackes', 'Granola', 'Chorizo Burrito', 'Fluffy Pancackes'];
+        console.log(breackfasts);
+    } else {
+        //var breackfasts = ['Cinnamon Doughnuts', 'Waffles', 'Granola', 'Chorizo Burrito', 'French Toast'];
+        //console.log(breackfasts);
+    }
+    
---
+++ SoloLearn/massive/www.js
@@ -0,0 +1,6 @@
+var breackfasts = ['Cinnamon Doughnuts', 'Waffles', 'Granola', 'Chorizo Burrito', 'French Toast'];
+var index = 0;
+    c = 'Fluffy Pancackes';
+    breackfasts[index] = c;
+    
+document.write(breackfasts);
